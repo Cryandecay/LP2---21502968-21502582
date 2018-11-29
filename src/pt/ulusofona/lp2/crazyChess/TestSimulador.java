02db6f2bc11086afe16d68324e6a95c95ada91f7
Cryandecay <38725656+Cryandecay@users.noreply.github.com>
2018-11-29 10:42:06 +0000

    Add files via upload

---
+++ src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
@@ -0,0 +1,28 @@
+package pt.ulusofona.lp2.crazyChess;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+
+class TestSimulador {
+    Simulador simulador = new Simulador();
+
+
+    @Test
+    void iniciaJogo() {
+        File ficheiro = new File("dados.txt");
+        assertEquals(simulador.iniciaJogo(ficheiro),true);
+    }
+
+
+
+    @Test
+    void processaJogada() {
+        CrazyPiece piece = new CrazyPiece("1","0","0","O Maior",3,2);
+        assertEquals(simulador.processaJogada(piece.getCoordenadaX(),piece.coordenadaY,4,3),false);
+    }
+
+}
